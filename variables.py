9b3917b3ba4687795ccd60b15ad7ddf4c9631d7e
Yuvraj Chaudhry <yuvraj.chaudhry@yahoo.com>
2026-04-07 00:30:14 +0900

    I did these today

---
+++ variables.py
@@ -0,0 +1,9 @@
+name="sophia"
+print(name)
+Religion="christainity"
+print(Religion)#wasn't necessary but i had to#
+hobbie="Running"
+print(hobbie)
+
+#combining text and number
+print("I was supposed to speak with my mummy at", 2, "am yesterday")
